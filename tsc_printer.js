0f12c15ebe0fc5ef90534197fb2f41c505c6dd3f
onewoorks <irwanbibrahim@onewoorks.com>
2019-03-12 01:42:02 +0800

    fixing code

--- tsc_printer.js
+++ tsc_printer.js
@@ -121,23 +121,22 @@ catch (error) {
 var te = "alaa";
 
 module.exports = {
-	sampleje: function(){
-		console.log('run sample')
-	},
-	printfile: function(){
-    var font_variable = { x: '50', y: '50', fonttype: '3', rotation: '0', xmul: '1', ymul: '1', text: 'Font Test' }
-    var barcode_variable = { x: '50', y: '100', type: '128', height: '70', readable: '0', rotation: '0', narrow: '3', wide: '1', code: '123456' }
-    var label_variable = { quantity: '1', copy: '1' };
-    openport('', true);
-    var status = printer_status(100, true);
-    clearbuffer('', true);
-    printerfont(font_variable, true);
-    barcode(barcode_variable, true);
-    sendcommand('TEXT 250,50,\"0\",0,10,10,\"Text Test!!\"', true);
-    printlabel(label_variable, true);
-    closeport('', true);
-}
+    sampleje: function () {
+        console.log('run sample')
+    },
+    printfile: function () {
+        var label_variable = { quantity: '1', copy: '1' };
+        openport('', true);
+        var status = printer_status(100, true);
+        clearbuffer('', true);
+        sendcommand('TEXT 250,23,"0",0,10,10,"FEED LINE 1 ABCDEFGHIJ"', true);
+        sendcommand('TEXT 250,43,"0",0,10,10,"FEED LINE 2 KLMNOPGRST"', true);
+        sendcommand('TEXT 250,63,"0",0,10,10,"FEED LINE 3 UVWXYZ...."', true);
+        sendcommand('BARCODE 100,23,"128",2,0,10,10,"NO TAG"', true);
+        printlabel(label_variable, true);
+        closeport('', true);
+    }
 
-	
+
 }
 
