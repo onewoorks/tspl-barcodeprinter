7cdac614ebb5f233ffafb372cd8acebf30ad6f65
onewoorks <irwanbibrahim@onewoorks.com>
2019-03-12 01:37:05 +0800

    update code

--- src/print_data.js
+++ src/print_data.js
@@ -1,14 +1,17 @@
+var tp = require('../tsc_printer')
+
 class PrintData {
 
     _looper(jsonData) {
         let data = jsonData
         jsonData.map((value,key) => {
             this._tspl(value)
+			tp.printfile()
         })
     }
 
     _tspl(value) {
-        console.log('TEXT 120, 20, "128", 100, 2, 0,2, 2 "'+ value.nama_stock +'"')
+        console.log('TEXT 120, 20, "128", 100, 2, 0,2, 2 "'+ value.nama_stok +'"')
         console.log('TEXT 120, 20, "128", 100, 2, 0,2, 2 "'+ value.no_tag +'"')
         console.log('TEXT 120, 20, "128", 100, 2, 0,2, 2 "'+ value.pembekal+'"')
     }
